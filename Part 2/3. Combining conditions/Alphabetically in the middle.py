43aed9afab4ca9336bc3f1fb355e8c01e1f7b62c
Dawid Pabis <dawidpabis1138@gmail.com>
2026-05-24 16:26:25 +0200

    finish chapter 3 of part 2

---
+++ Part 2/3. Combining conditions/Alphabetically in the middle.py
@@ -0,0 +1,16 @@
+letter1 = input("1st letter: ")
+letter2 = input("2nd letter: ")
+letter3 = input("3rd letter: ")
+if letter1 > letter2 and letter1 > letter3:
+    if letter2 > letter3:
+        middle = letter2
+    else:
+        middle = letter3
+elif letter1 < letter2 and letter1 < letter3:
+    if letter2 > letter3:
+        middle = letter3
+    else:
+        middle = letter2
+else:
+    middle = letter1
+print(f"The letter in the middle is {middle}")
